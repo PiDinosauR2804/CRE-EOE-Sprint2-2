adc840c204c8bf45475c877e15bdb8bdecde3e3e
PiDinosaur2804 <ngodinhluyennht@gmail.com>
2025-02-08 11:58:32 +0700

    update

--- main.py
+++ main.py
@@ -110,13 +110,6 @@ def main(cfg: DictConfig):
 
         model = task_to_model[args.model_name](args)
         
-        # Multi GPU
-        if torch.cuda.device_count() > 1:
-            print("Let's use", torch.cuda.device_count(), "GPUs!")
-            # dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
-            model = nn.DataParallel(model)
-
-        
         model.to(args.device)
 
         trainer = task_to_trainer[args.trainer_name](args=args)
